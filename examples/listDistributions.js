fb929194903321a938fd3dc2adbfbdb19d073e07
Christian Tellnes <christian@tellnes.no>
2012-04-25 20:48:54 +0200

    set streaming: false in listDistributions example

--- examples/listDistributions.js
+++ examples/listDistributions.js
@@ -2,7 +2,7 @@ var cloudfront = require('..');
 
 var cf = cloudfront.createClient(process.env.AWS_KEY, process.env.AWS_SECRET);
 
-cf.listDistributions({streaming: true}, function(err, list, info) {
+cf.listDistributions({streaming: false}, function(err, list, info) {
   if (err) {
     console.error(err);
   } else {
