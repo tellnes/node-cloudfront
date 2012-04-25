4c52f84c30b875e5e44820e7ae6a0b01c44b90ad
Christian Tellnes <christian@tellnes.no>
2012-04-25 18:13:29 +0200

    update invalidation examples

--- examples/invalidate1.js
+++ examples/invalidate1.js
@@ -4,8 +4,8 @@ var cf = cloudfront.createClient(process.env.AWS_KEY, process.env.AWS_SECRET);
 
 // argv: distributionId, callerReference, path
 
-cf.createInvalidation(process.argv[2], process.argv[2], process.argv[3], function(err, invalidation) {
-  if (err) throw err;
+cf.createInvalidation(process.argv[2], process.argv[3], process.argv[4], function(err, invalidation) {
+  if (err) return console.error(err);
 
   console.log(invalidation);
 });
--- examples/invalidate2.js
+++ examples/invalidate2.js
@@ -5,10 +5,10 @@ var cf = cloudfront.createClient(process.env.AWS_KEY, process.env.AWS_SECRET);
 // argv: distributionId, callerReference, path
 
 cf.getDistribution(process.argv[2], function(err, distribution) {
-  if (err) throw err;
+  if (err) return console.error(err);
 
-  distribution.invalidate(process.argv[2], process.argv[3], function(err, invalidation) {
-    if (err) throw err;
+  distribution.invalidate(process.argv[3], process.argv[4], function(err, invalidation) {
+    if (err) return console.error(err);
 
     console.log(invalidation);
   });
